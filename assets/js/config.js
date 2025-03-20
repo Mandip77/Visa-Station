76190eace3ffa119018bc13df21505f74c6270f2
Mandip Amgain <mandip.amgain123@gmail.com>
2025-03-19 22:46:35 -0400

    Fix CSS & JS loading for GitHub Pages

--- assets/js/config.js
+++
@@ -1,4 +0,0 @@
-// filepath: /c:/Users/mandi/OneDrive/Desktop/github foundation/learning-git-github-2421501-main/Visa Station/config.js
-const EMAILJS_USER_ID = 'PMaw25qBEK3t7mSGP';
-const EMAILJS_SERVICE_ID = 'service_dc0a03k';
-const EMAILJS_TEMPLATE_ID = 'template_0o8yjvd';
